dd0afc8a286bfaf15601575441acd9eca5b3fb19
rmahajan466 <rmahajan466@gmail.com>
2025-01-20 15:55:54 +0530

    Added UI Table

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import fetchCoinData from "../services/fetchCoinData";
 import { useQuery } from "react-query";
 
@@ -6,28 +6,83 @@ function CoinTable() {
 
     const [page, setPage] = useState(1);
     const { data, isLoading, isError, error, isFetching } = useQuery(['coins',page], () => fetchCoinData(page, 'usd'), {
-        retry: 2,
-        retryDelay: 1000,
+        // retry: 2,
+        // retryDelay: 1000,
         cacheTime: 1000 * 60 * 2,
+        staleTime: 1000 * 60 * 2,
     });
 
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
-
-    if(isLoading){
-        return <div> Loading... </div> ;
-    }
     if(isError){
         return <div> Error: {error.message} </div> ;
     }
 
     return (
-        <>
-            Coin Table
-            <button onClick={() => setPage(page+1)}>Click</button>
-            {page}
-        </>
+        <div className="flex flex-col items-center justify-center gap-5 my-5 w-[80vw] mx-auto">
+            <div className="flex items-center justify-center w-full px-2 py-4 font-semibold text-black bg-yellow-400">
+                {/* Hear of the Table */}
+                <div className="basis-[35%]">
+                    Coin
+                </div>
+                <div className="basis-[25%]">
+                    Price
+                </div>
+                <div className="basis-[20%]">
+                    24H Change
+                </div>
+                <div className="basis-[20%]">
+                    Marker Cap
+                </div>
+            </div>
+
+            <div className="flex flex-col w-[80vw] mx-auto">
+                {isLoading && <div> Loading... </div>}
+                {data && data.map((coin) => {
+                    return(
+                        <div key={coin.id} className="flex items-center justify-between w-full px-2 py-4 font-semibold text-white bg-transparent">
+                            <div className="flex items-center gap-3 justify-start basis-[35%]">
+
+                                <div className="w-[5rem] h-[5rem]">
+                                    <img src={coin.image} className="w-full h-full" />
+                                </div>
+
+                                <div className="flex flex-col">
+                                    <div className="text-3xl"> {coin.name} </div>
+                                    <div className="text-xl"> {coin.symbol} </div>
+                                </div>
+
+
+                            </div>
+
+                            <div className="basis-[25%]">
+                                {coin.current_price}
+                            </div>
+                            <div className="basis-[20%]">
+                                {coin.price_change_24h}
+                            </div>
+                            <div className="basis-[20%]">
+                                {coin.market_cap}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <div className="flex items-center justify-center gap-4">
+                <button
+                disabled = {page === 1}
+                    onClick={() => setPage(page - 1)}
+                    className="text-2xl text-white btn btn-primary btn-wide"
+                >
+                    Prev
+                </button>
+                <button
+                    onClick={() => setPage(page + 1)}
+                    className="text-2xl text-white btn btn-secondary btn-wide"
+                >
+                    Next
+                </button>
+            </div>
+        </div>
     );
 }
 
