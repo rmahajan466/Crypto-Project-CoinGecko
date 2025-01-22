d4fea3e9419037bda43ff59b349858489f74a955
rmahajan466 <rmahajan466@gmail.com>
2025-01-22 13:55:01 +0530

    Added home page at nav bar click

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import currencyStore from "../../state/store";
 
 function Navbar() {
 
     const {setCurrency} = currencyStore();
+    const navigate = useNavigate();
+
+    function goToHome(){
+        navigate("/");
+    }
 
     return (
         <div className="navbar bg-base-100">
@@ -30,7 +36,7 @@ function Navbar() {
                     </ul>
                 </div>
             </div>
-            <div className="navbar-center">
+            <div onClick={goToHome} className="navbar-center">
                 <a className="text-xl btn btn-ghost">Cryto Tracker</a>
             </div>
             <div className="navbar-end">
